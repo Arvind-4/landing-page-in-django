b97835b184f4a1b94d2ea6084e49bf0f9f79f961
Arvind-4 <lordarvindthegamer@gmail.com>
2021-08-19 17:28:49 +0530

    Updated Postgres db for Production

--- src/settings/production.py
+++ src/settings/production.py
@@ -1,6 +1,7 @@
 from .base import *
 import os
 import django_heroku
+import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -9,5 +10,7 @@ DEBUG = os.environ.get('DEBUG')
 ALLOWED_HOSTS = ['*']
 
 ADMIN_URL = os.environ.get('ADMIN_URL')
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 django_heroku.settings(locals())
