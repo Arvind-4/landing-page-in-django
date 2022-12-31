822fc479d67a8ea2eb3a14874eef762361b6d7c4
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-31 16:18:11 +0530

    Updated: added debug messages

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -3,7 +3,9 @@ from decouple import config
 DJANGO_LIVE = config('DJANGO_LIVE', cast=bool)
 
 if DJANGO_LIVE:
+    print("Production settings loaded")
     from .production import *
 
 else:
+    print("Development settings loaded")
     from .local import *
